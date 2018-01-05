abd7ee7ff7ee2921d4f6761e54e3fce067451018
mpascariu <xpascariu@outlook.com>
2018-01-05 17:26:41 +1100

    update vignette

--- inst/doc/Installation.R
+++ inst/doc/Installation.R
@@ -2,9 +2,11 @@
 knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  install.packages("MortalityLaws")
+#      install.packages("MortalityLaws")
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  # install.packages("devtools")
-#  devtools::install_github("mpascariu/MortalityLaws")
+#      install.packages("devtools")
+
+## ---- eval=FALSE---------------------------------------------------------
+#      devtools::install_github("mpascariu/MortalityLaws")
 
