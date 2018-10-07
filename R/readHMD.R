7e9ace1c4448db01b5030f9707bd455edcee05b2
Marius Pascariu <rpascariu@outlook.com>
2018-10-07 03:21:01 +0200

    expand ReadHMD to download all types of cohort data as well. Add 2 more error mesages and tests.

--- R/readHMD.R
+++ R/readHMD.R
@@ -4,15 +4,33 @@
 #' and regions in a single object from the \href{https://www.mortality.org}{
 #' Human Mortality Database}.
 #' 
-#' @param what What type of data are you looking for? There are available:
-#' birth records \code{"births"}, death counts \code{"Dx"}, 
-#' deaths by Lexis triangles \code{"lexis"}, population size \code{"population"},
-#' exposure-to-risk \code{"Ex"}, death-rates \code{"mx"}, 
-#' life tables for females \code{"LT_f"}, life tables for males \code{"LT_m"},
-#' life tables both sexes combined \code{"LT_t"}, 
-#' life expectancy at birth \code{"e0"},
-#' cohort death-rates \code{"mxc"} and cohort exposures \code{"Exc"}. 
-#' @param countries HMD country codes.
+#' @param what What type of data are you looking for? The following options are 
+#' available: \itemize{
+#'   \item{\code{"births"}} -- birth records;
+#'   \item{\code{"Dx_lexis"}} -- deaths by Lexis triangles;
+#'   \item{\code{"Ex_lexis"}} -- exposure-to-risk by Lexis triangles;
+#'   \item{\code{"population"}} -- population size;
+#'   \item{\code{"Dx"}} -- death counts;
+#'   \item{\code{"Ex"}} -- exposure-to-risk;
+#'   \item{\code{"mx"}} -- central death-rates;
+#'   \item{\code{"LT_f"}} -- period life tables for females;
+#'   \item{\code{"LT_m"}} -- period life tables for males;
+#'   \item{\code{"LT_t"}} -- period life tables both sexes combined;
+#'   \item{\code{"e0"}} -- period life expectancy at birth;
+#'   \item{\code{"Exc"}} -- cohort exposures;
+#'   \item{\code{"mxc"}} -- cohort death-rates;
+#'   \item{\code{"LT_fc"}} -- cohort life tables for females;
+#'   \item{\code{"LT_mc"}} -- cohort life tables for males;
+#'   \item{\code{"LT_tc"}} -- cohort life tables both sexes combined;
+#'   \item{\code{"e0c"}} -- cohort life expectancy at birth;
+#'   }
+#' @param countries Specify the country data you want to download by adding the 
+#' HMD country code/s. Options: 
+#' \code{"AUS","AUT","BEL","BGR","BLR","CAN","CHL","CHE","CZE", "DEUTE",
+#' "DEUTNP","DEUTW","DNK","ESP","EST","FIN","FRACNP","FRATNP","KOR","GBR_NIR",
+#' "GBR_NP","GBR_SCO","GBRCENW","GBRTENW","GRC","HUN","HRV","IRL","ISL","ISR",
+#' "ITA","JPN","LTU","LUX","LVA","NLD","NOR","NZL_MA","NZL_NM","NZL_NP","POL",
+#' "PRT","RUS","SVK","SVN","SWE","TWN","USA","UKR"}.
 #' @param interval HMD data format: (age interval x year interval).
 #' Interval options: \code{1x1}, \code{1x5}, \code{1x10}, 
 #' \code{5x1}, \code{5x5}, \code{5x10}.
@@ -21,7 +39,8 @@
 #' @param password Your HMD password.
 #' @param save Do you want to save a copy of the dataset on your local machine? 
 #' Logical. Default: \code{FALSE}.
-#' @param show Choose whether to display a progress bar. Logical. Default: \code{TRUE}.
+#' @param show Choose whether to display a progress bar. Logical. 
+#' Default: \code{TRUE}.
 #' @return A \code{ReadHMD} object that contains:
 #'  \item{input}{List with the input values (except the password).}
 #'  \item{data}{Data downloaded from HMD.}
@@ -61,24 +80,36 @@
 ReadHMD <- function(what, countries = NULL, interval = "1x1",  
                     username, password, save = FALSE, show = TRUE){
   # Step 1 - Validate input & Progress bar setup
-  if (is.null(countries)) countries <- HMDcountries()
+  if (is.null(countries)) {
+    countries <- HMDcountries()
+  }
+  
   input <- list(what = what, countries = countries, interval = interval, 
                 username = username, save = save, show = show)
   check_input_ReadHMD(input)
   nr <- length(countries)
-  if (show) {pb <- startpb(0, nr + 1); on.exit(closepb(pb)); setpb(pb, 0)}
+  
+  if (show) {
+    pb <- startpb(0, nr + 1)
+    on.exit(closepb(pb))
+    setpb(pb, 0)
+  }
   
   # Step 2 - Do the loop for the other countries
   D <- data.frame() 
   for (i in 1:nr) {
-    if (show) {setpb(pb, i); cat(paste("      :Downloading", countries[i], "    "))}
-    D <- rbind(D, ReadHMD.core(what, country = countries[i], 
-                               interval, username, password))
+    if (show) {
+      setpb(pb, i)
+      cat(paste("      :Downloading", countries[i], "    "))
+    }
+    
+    D <- rbind(D, ReadHMD.core(what, country = countries[i], interval, 
+                               username, password))
   }
-  Y   <- sort(unique(D$Year))
-  A   <- sort(unique(D$Age))
   fn  <- paste0("HMD_", what) # file name
-  out <- list(input = input, data = D, download.date = date(), years = Y, ages = A)
+  out <- list(input = input, data = D, download.date = date(), 
+              years = sort(unique(D$Year)), 
+              ages = unique(D$Age))
   out <- structure(class = "ReadHMD", out)
   
   # Step 3 - Write a file with the database in your working directory
@@ -86,43 +117,62 @@ ReadHMD <- function(what, countries = NULL, interval = "1x1",
     assign(fn, value = out)
     save(list = fn, file = paste0(fn, ".Rdata"))
   }
+  
   if (show) {
     setpb(pb, nr + 1)
     wd  <- getwd()
     n   <- nchar(wd)
     wd_ <- paste0("...", substring(wd, first = n - 45, last = n))
     cat("\n   ")
+    
     if (save) {
-      message(paste(fn, "is saved in your working directory:\n  ", wd_), appendLF = F)
+      message(paste(fn, "is saved in your working directory:\n  ", wd_), 
+              appendLF = FALSE)
       cat("\n   ")
     }
+    
     message("Download completed!")
   }
+  
+  # Exit
   return(out)
 }
+
 
 #' Function to Download Data for a one Country
 #' @inheritParams ReadHMD
 #' @param country HMD country code for the selected country. Character.
 #' @keywords internal
 ReadHMD.core <- function(what, country, interval, username, password){
-  if (what == 'e0' & interval == '1x1') {
-    whichFile = 'E0per.txt' } else {
-      whichFile <- switch(what, 
-                          births = paste0("Births.txt"),
-                          population = paste0("Population.txt"),
-                          lexis = paste0("Deaths_lexis.txt"),
-                          Dx   = paste0("Deaths_", interval, ".txt"),    # deaths
-                          Ex   = paste0("Exposures_", interval, ".txt"), # exposure
-                          mx   = paste0("Mx_", interval, ".txt"),        # death rates
-                          LT_f = paste0("fltper_", interval, ".txt"),    # Life tables, Females
-                          LT_m = paste0("mltper_", interval, ".txt"),    # Life tables, Males
-                          LT_t = paste0("bltper_", interval, ".txt"),    # Life tables, Both sexes
-                          e0   = paste0("E0per_", interval, ".txt"),     # Life expectancy
-                          # Cohort data
-                          mxc = paste0("cMx_", interval, ".txt"),    # deaths
-                          Exc = paste0("cExposures_", interval, ".txt") # exposure
-      )}
+  
+  if (what == "e0" & interval == "1x1") {
+    whichFile <- "E0per.txt" 
+    
+  } else if (what == "e0c" & interval == "1x1"){
+    whichFile <- "E0coh.txt"
+    
+  } else {
+    whichFile <- switch(what, 
+                        births = "Births.txt",
+                        population = "Population.txt",
+                        Dx_lexis = "Deaths_lexis.txt",
+                        Ex_lexis = "Exposures_lexis.txt",
+                        Dx   = paste0("Deaths_", interval, ".txt"),    # deaths
+                        Ex   = paste0("Exposures_", interval, ".txt"), # exposure
+                        mx   = paste0("Mx_", interval, ".txt"),        # death rates
+                        LT_f = paste0("fltper_", interval, ".txt"),    # Life tables, Females
+                        LT_m = paste0("mltper_", interval, ".txt"),    # Life tables, Males
+                        LT_t = paste0("bltper_", interval, ".txt"),    # Life tables, Both sexes
+                        e0   = paste0("E0per_", interval, ".txt"),     # Life expectancy
+                        # Cohort data
+                        Exc = paste0("cExposures_", interval, ".txt"),
+                        mxc = paste0("cMx_", interval, ".txt"),
+                        LT_fc = paste0("fltcoh_", interval, ".txt"),
+                        LT_mc = paste0("mltcoh_", interval, ".txt"),
+                        LT_tc = paste0("bltcoh_", interval, ".txt"),
+                        e0c = paste0("E0coh_", interval, ".txt")
+    )}
+  
   path <- paste0("https://www.mortality.org/hmd/", country, "/STATS/", whichFile)
   txt  <- RCurl::getURL(path, userpwd = paste0(username, ":", password))
   con  <- try(textConnection(txt), 
@@ -135,20 +185,23 @@ ReadHMD.core <- function(what, country, interval, username, password){
                    call. = FALSE))
   close(con)
   out <- cbind(country, dat)
-  if (interval == "1x1" & !(what %in% c('births', 'lexis', 'e0')) ) out$Age = 0:110
+  if (interval %in% c("1x1", "1x5", "1x10") & 
+      !(what %in% c("births", "Dx_lexis", "Ex_lexis", "e0", "e0c"))) {
+    out$Age <- 0:110
+  }
   return(out)
 }
+
 
 #' Country codes
 #' @keywords internal
 HMDcountries <- function() {
-  HMDc <- c("AUS","AUT","BEL","BGR","BLR","CAN","CHL","CHE","CZE",
-            "DEUTE","DEUTNP","DEUTW","DNK","ESP","EST","FIN","FRACNP","FRATNP",
-            "KOR","GBR_NIR","GBR_NP","GBR_SCO","GBRCENW","GBRTENW","GRC",
-            "HUN","HRV","IRL","ISL","ISR","ITA","JPN","LTU","LUX","LVA","NLD",
-            "NOR","NZL_MA","NZL_NM","NZL_NP","POL","PRT","RUS","SVK",
-            "SVN","SWE","TWN","USA","UKR")
-  HMDc
+  c("AUS","AUT","BEL","BGR","BLR","CAN","CHL","CHE","CZE",
+    "DEUTE","DEUTNP","DEUTW","DNK","ESP","EST","FIN","FRACNP","FRATNP",
+    "KOR","GBR_NIR","GBR_NP","GBR_SCO","GBRCENW","GBRTENW","GRC",
+    "HUN","HRV","IRL","ISL","ISR","ITA","JPN","LTU","LUX","LVA","NLD",
+    "NOR","NZL_MA","NZL_NM","NZL_NP","POL","PRT","RUS","SVK",
+    "SVN","SWE","TWN","USA","UKR")
 }
 
 
@@ -156,41 +209,76 @@ HMDcountries <- function() {
 #' @param x a list containing the input arguments from ReadHMD function
 #' @keywords internal
 check_input_ReadHMD <- function(x) {
-  int <- c('1x1', '1x5', '1x10', '5x1', '5x5','5x10')
-  wht <- c('births', 'population', 'lexis', 'Dx', 'Ex', 'mx', 
-           'LT_f', 'LT_m', 'LT_t', 'e0', 'mxc', 'Exc')
+  int <- c("1x1", "1x5", "1x10", "5x1", "5x5","5x10")
+  wht <- c("births", "population", "Dx_lexis", "Ex_lexis", "Dx", 
+           "mx", "Ex", "LT_f", "LT_m", "LT_t", "e0", 
+           "mxc", "Exc", "LT_fc", "LT_mc", "LT_tc", "e0c")
+  all_countries <- HMDcountries()
+  coh_countries <- c("DNK", "FIN", "FRATNP", "FRACNP", "ISL", "ITA", "NLD", 
+                     "NOR", "SWE", "CHE", "GBRTENW", "GBRCENW", "GBR_SCO")
   
   if (!(x$interval %in% int)) {
-    stop('The interval ', x$interval, ' does not exist in HMD ', 
-         'Try one of these options:\n', paste(int, collapse = ', '), 
-         call. = F)}
+    stop("The interval ", x$interval, " does not exist in HMD ", 
+         "Try one of these options:\n", paste(int, collapse = ", "), 
+         call. = FALSE)
+  }
+  
   if (!(x$what %in% wht)) {
-    stop(x$what, ' does not exist in HMD. Try one of these options:\n', 
-         paste(wht, collapse = ', '), call. = F)}
-  if (all(!(x$countries %in% HMDcountries())) ) {
-    stop('Something is wrong in the country/countries added by you.\n',
-         'Try one or more of these options:\n', 
-          paste(HMDcountries(), collapse = ', '), call. = F)}
+    stop(x$what, " does not exist in HMD. Try one of these options:\n", 
+         paste(wht, collapse = ", "), call. = FALSE)
+  }
+  
+  if (all(!(x$countries %in% all_countries))) {
+    stop("Something is wrong in the country/countries added by you.\n",
+         "Try one or more of these options:\n", 
+         paste(HMDcountries(), collapse = ", "), call. = FALSE)
+  }
+  
+  # Availability of Cohort Data
+  if ((x$what %in% c("LT_fc", "LT_mc", "LT_tc", "e0c")) & 
+      !(all(x$countries %in% coh_countries))) {
+    stop("Data type ", x$what, 
+         " is not available for one or more countries specified in input.\n", 
+         "Check one of these countries:\n", 
+         paste(coh_countries, collapse = ", "), call. = FALSE)
+  }
+  
+  # Availability of Life Expectancy Data
+  if ((x$what %in% c("e0", "e0c")) & 
+      !(x$interval %in% c("1x1", "1x5", "1x10"))) {
+    stop("Data type ", x$what, 
+         " is available only in the following formats: '1x1', '1x5', '1x10'.", 
+         call. = FALSE)
+  }
 }
 
 
 #' Print ReadHMD
-#' @param x an object of class \code{"ReadHMD"}
-#' @param ... further arguments passed to or from other methods.
+#' @param x An object of class \code{"ReadHMD"}
+#' @param ... Further arguments passed to or from other methods.
 #' @keywords internal
 #' @export
 print.ReadHMD <- function(x, ...){
   what <- x$input$what
-  cat('Human Mortality Database (www.mortality.org)\n')
-  cat('Downloaded by :', x$input$username, '\n')
-  cat('Download Date :', x$download.date, '\n')
-  cat('Type of data  :', what, '\n')
+  cat("Human Mortality Database (www.mortality.org)\n")
+  cat("Downloaded by :", x$input$username, "\n")
+  cat("Download Date :", x$download.date, "\n")
+  cat("Type of data  :", what, "\n")
   cat(paste("Interval      :", x$input$interval, "\n"))
-
-  ageMsg <- if (what == "e0") 0 else paste(min(x$ages), "-", max(x$ages))
-  cat(paste("Years         :", min(x$years), "-", max(x$years), "\n"))
+  
+  if (what %in% c("e0", "e0c")) {
+    ageMsg <- 0
+    
+  } else {
+    # ageMsg <- paste(min(x$ages), "-", max(x$ages))
+    ageMsg <- paste(x$ages[1], "--", rev(x$ages)[1])
+  }
+  
+  cat(paste("Years         :", x$years[1], "--", rev(x$years)[1], "\n"))
   cat(paste("Ages          :", ageMsg, "\n"))
   cat("Countries     :", x$input$countries, "\n")
-  cat('\nData:\n')
+  cat("\nData:\n")
   print(head_tail(x$data, hlength = 5, tlength = 5))
 }
+
+
--- tests/testthat/test_ReadHMD.R
+++ tests/testthat/test_ReadHMD.R
@@ -1,12 +1,28 @@
 rm(list = ls())
 
-expect_error(ReadHMD(what = "Dx", username = "fake_user", password = "fake_password"))
+# Wrong user & password
 expect_error(availableHMD(username = "fake_user", password = "fake_password"))
+expect_error(ReadHMD(what = "Dx", username = "fake_user", password = "fake_password"))
 
-expect_error(ReadHMD(what = "DxDD", username = "username", password = "password"))
+# Wrong index
+expect_error(ReadHMD(what = "DxDD", 
+                     username = "username", password = "password"))
+# Wrong country
 expect_error(ReadHMD(what = "Dx", countries = "AUSS", 
                      username = "username", password = "password"))
+
+# Wrong interval
 expect_error(ReadHMD(what = "Dx", countries = "AUS", interval = "1x50", 
+                     username = "username", password = "password"))
+
+# Wrong country for the index
+expect_error(ReadHMD(what = "LT_fc", countries = "AUS", interval = "1x1", 
+                     username = "username", password = "password"))
+
+# Wrong interval for the index
+expect_error(ReadHMD(what = "e0", countries = "SWE", interval = "5x1", 
+                     username = "username", password = "password"))
+expect_error(ReadHMD(what = "e0c", countries = "SWE", interval = "5x1", 
                      username = "username", password = "password"))
 
 
